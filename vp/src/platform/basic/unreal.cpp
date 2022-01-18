12a79cade9b812e33a37ee00f65a19ca263512da
Christian Langolf <christian.langolf@engram.de>
2022-01-18 11:09:27 +0000

    cleanup

--- vp/src/platform/basic/unreal.cpp
+++ vp/src/platform/basic/unreal.cpp
@@ -11,36 +11,13 @@ Unreal::Unreal(sc_module_name) {
 	tsock.register_b_transport(this, &Unreal::transport);
 }
 
-static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
-}
-
 void Unreal::transport(tlm::tlm_generic_payload &trans, sc_core::sc_time &delay) {
     std::cout << "Hello from Unreal!" << std::endl;
 
     CURL *curl;
     CURLcode res;
     long http_code;
-    std::string readBuffer;
 
-/*     if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "host.docker.internal:30010/remote/info");
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L); 
-        res = curl_easy_perform(curl);
-
-
-        if (res != CURLE_OK){
-            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
-        }
-
-        curl_easy_cleanup(curl);
-
-        std::cout << readBuffer << std::endl;
-    } */
-    
     json j = 
     {
         {"objectPath", "/Game/FirstPersonBP/Maps/FirstPersonExampleMap.FirstPersonExampleMap:PersistentLevel.PointLight_1.LightComponent0"},
@@ -51,30 +28,16 @@ void Unreal::transport(tlm::tlm_generic_payload &trans, sc_core::sc_time &delay)
         {"generateTransaction", true}
     };
 
-/*     std::string j = R"(
-        {
-            "objectPath" : "/Game/FirstPersonBP/Maps/FirstPersonExampleMap.FirstPersonExampleMap:PersistentLevel.PointLight_1.LightComponent0",
-            "functionName" : "SetIntensity",
-            "parameters" : {
-                "NewIntensity" : 10.0
-            },
-            "generateTransaction" : true
-        }
-        )"; */
-
-    std::cout << j << std::endl;
-
     curl = curl_easy_init();
     curl_global_init(CURL_GLOBAL_ALL);
 
-    if(curl){
-
+    if (curl) {
         struct curl_slist *headers = NULL;
         headers = curl_slist_append(headers, "Accept: application/json");
         headers = curl_slist_append(headers, "Content-Type: application/json");
         headers = curl_slist_append(headers, "charset: utf-8");
 
-        curl_easy_setopt(curl, CURLOPT_URL, "http://host.docker.internal:30010/remote/object/call");
+        curl_easy_setopt(curl, CURLOPT_URL, "host.docker.internal:30010/remote/object/call");
         curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, j.dump().c_str());
@@ -85,7 +48,7 @@ void Unreal::transport(tlm::tlm_generic_payload &trans, sc_core::sc_time &delay)
 
         res = curl_easy_perform(curl);
 
-        if (res != CURLE_OK){
+        if (res != CURLE_OK) {
             std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
         }
 
@@ -96,7 +59,5 @@ void Unreal::transport(tlm::tlm_generic_payload &trans, sc_core::sc_time &delay)
 
         curl_easy_cleanup(curl);
         curl_global_cleanup();
-
     }
-    
 }
