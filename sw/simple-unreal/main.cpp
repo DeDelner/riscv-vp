28dbdbf3f73c933f18c464d31359460c2234cfdb
DeDelner <dedelner@outlook.com>
2022-03-28 17:25:57 +0000

    PID Controller

--- sw/simple-unreal/main.cpp
+++ sw/simple-unreal/main.cpp
@@ -4,13 +4,11 @@
 #include <iomanip>
 #include <cmath>
 #include <bit>
+#include <stdio.h>
+#include "pid.h"
 #include "unreal.hpp"
 
 using namespace std;
-
-void wait(uint32_t time) {
-	for (uint32_t i = 128 * time; i > 0; i--) {}
-}
 
 void set_electromagnet_power(uint32_t power) {
     Unreal::execute(
@@ -29,10 +27,6 @@ uint32_t get_hallsensor_value() {
     return Unreal::read_unreal_data();
 }
 
-float ieee_float(uint32_t f) {
-  return std::bit_cast<float>(f);
-}
-
 int main() {
 
     const int32_t THRESHOLD = 50;
@@ -43,24 +37,34 @@ int main() {
 
     int32_t velocity = 0;
 
+    // Kp -  proportional gain
+    // Ki -  Integral gain
+    // Kd -  derivative gain
+    // dt -  loop interval time
+    // max - maximum value of manipulated variable
+    // min - minimum value of manipulated variable
+    double dt = 0.0000001;
+    double max = 1.0;
+    double min = -1.0;
+    double Kp = 0.04;
+    double Kd = 1;
+    double Ki = 1;
+    PID pid = PID(dt, max, min, Kp, Kd, Ki);
+
+    int setpoint = 30;
+
+    int val = get_hallsensor_value();
     while (true) {
-        velocity = hallsensor_value - previous_hallsensor_value;
-        previous_hallsensor_value = hallsensor_value;
-
-        int32_t wait_time = clamp((hallsensor_value - THRESHOLD) * 16, (int32_t)46 - velocity, (int32_t)1000);
-
-        int32_t height = max(hallsensor_value - 28, (int32_t)0);
-
-            set_electromagnet_power(500);
-            wait(ELEVATION_THRESHOLD - height - velocity);
-            set_electromagnet_power(0);
-            //wait(max(velocity * 6, (int32_t)0));
-
-        std::cout << "height: " << height << std::endl;
+        int inc = (int)pid.calculate(setpoint, get_hallsensor_value());
+        std::cout << "val: " << val << ", inc: " << inc << std::endl;
         std::cout << "\033c"; // clear line
+        val += inc * inc *inc;
 
-
-        hallsensor_value = get_hallsensor_value();
+        if (val > 0) {
+            set_electromagnet_power(500);
+        } else {
+            set_electromagnet_power(0);
+        }
     }
 
 	return 0;
